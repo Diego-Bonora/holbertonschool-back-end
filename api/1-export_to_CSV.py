c7c1b89de30c621f345e0939662e91ae1f2ce1e1
Diego-Bonora <bonoradi@gmail.com>
2023-08-21 10:12:27 -0700

    write data from jsonplaceholder to a csv file

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+requests TODO from jsonplaceholder and
+creates a csv with the info
+"""
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    USER = sys.argv[1]
+    USER_INFO = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(
+            USER)).json()
+    TODO = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            USER)).json()
+
+    list_to_csv = []
+    for values in TODO:
+        new_list = [str(USER_INFO["id"]), str(USER_INFO["name"]),
+                    str(values["completed"]), str(values["title"])]
+        list_to_csv.append(new_list)
+
+    with open("{}.csv".format(USER), mode="w", encoding="UTF8") as file:
+        writer = csv.writer(file)
+        writer.writerows(list_to_csv)
