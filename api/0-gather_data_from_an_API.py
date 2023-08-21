410928ba6a0d74abe76f8cb190ef6f3e9dffeadc
Diego-Bonora <bonoradi@gmail.com>
2023-08-21 09:21:54 -0700

    fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-""" getting todo list of users from an api  """
-
+"""
+requests TODO from jsonplaceholder and
+prints the employee with all the tasks done
+"""
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    """ getting todo list of users from an api  """
-    n = sys.argv[1]
-    resUser = requests.get(f'https://jsonplaceholder.typicode.com/users/{n}')
-    response = resUser.json()
-    resTask = requests.get('https://jsonplaceholder.typicode.com/todos')
-    responseTask = resTask.json()
-    done = 0
-    total = 0
-    doneTask = []
-    for task in responseTask:
-        if task["userId"] == int(n):
-            if task["completed"] is True:
-                doneTask.append(task["title"])
-                done += 1
-            else:
-                total += 1
+    USER = sys.argv[1]
+    USER_INFO = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(
+            USER)).json()
+    TODO = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            USER)).json()
+    EMPLOYEE_NAME = USER_INFO["name"]
+    TOTAL_NUMBER_OF_TASKS = len(TODO)
+    NUMBER_OF_DONE_TASKS = 0
+    tasks_done = ""
+    for tasks in TODO:
+        if tasks["completed"]:
+            tasks_done += "\t {}\n".format(tasks["title"])
+            NUMBER_OF_DONE_TASKS += 1
 
-    total += done
-
-    print(f"Employee {response['name']} is done with tasks({done}/{total}):")
-    for task in doneTask:
-        print(f"\t {task}")
+    info = "Employee {} is done with tasks({}/{}):\n".format(
+        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
+        TOTAL_NUMBER_OF_TASKS) + tasks_done[:-1]
+    print(info)
